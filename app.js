983112b4a1b9ee2b87d4b74d585f3031497e504f
Phillip Nguyen <nh273@cornell.edu>
2020-01-14 16:37:09 -0500

    separating out the handlemessage method

--- app.js
+++ app.js
@@ -19,7 +19,9 @@
  *
  */
 
-"use strict";
+import { handleMessage } from "./interactions/message.js";
+
+("use strict");
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const request = require("request"),
@@ -85,52 +87,6 @@ app.get("/webhook", (req, res) => {
     }
   }
 });
-
-function handleMessage(sender_psid, received_message) {
-  let response;
-
-  // Checks if the message contains text
-  if (received_message.text) {
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      text: `You sent the message: "${received_message.text}". Now send me an attachment!`
-    };
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [
-            {
-              title: "Is this the right picture?",
-              subtitle: "Tap a button to answer.",
-              image_url: attachment_url,
-              buttons: [
-                {
-                  type: "postback",
-                  title: "Yes!",
-                  payload: "yes"
-                },
-                {
-                  type: "postback",
-                  title: "No!",
-                  payload: "no"
-                }
-              ]
-            }
-          ]
-        }
-      }
-    };
-  }
-
-  // Send the response message
-  callSendAPI(sender_psid, response);
-}
 
 function handlePostback(sender_psid, received_postback) {
   console.log("ok");
