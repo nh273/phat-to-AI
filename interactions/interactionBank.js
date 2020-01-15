cc1a5c02b78e0295d1a100bdfbda81023e48d959
Phillip Nguyen <nh273@cornell.edu>
2020-01-15 10:33:57 -0500

    Adding interaction models

--- interactions/interactionBank.js
+++ interactions/interactionBank.js
@@ -29,35 +29,76 @@ const initialResponse = {
 
 const bank = [
   {
+    input_contains: ["h1b", "visa"],
+    responses: [
+      {
+        content: { text: "Con yên tâm, để thầy làm việc với anh Trump nhé!" },
+        prob: 1
+      }
+    ]
+  },
+  {
     input_contains: ["buồn", "khổ", "đau", "chán"],
     responses: [
       {
         content: { text: "Đời là bể khổ, quay đầu lại là bờ con ạ" },
-        prob: 0.5
+        prob: 0.25
       },
-      { content: { text: "Mô phật" }, prob: 0.5 }
+      { content: { text: "Mô phật" }, prob: 0.25 },
+      { content: { text: "Thiện tai, thiện tai" }, prob: 0.25 },
+      { content: { text: "Toang như con thì thầy cũng buồn" }, prob: 0.25 }
     ]
   },
   {
     input_contains: ["cúng", "lễ", "công đức", "tiền"],
     responses: [
       {
-        content: { text: "Đủ 300 tỷ là thầy xin giáo hội cho hoàn tục!" },
+        content: {
+          text: "Dành dụm đủ 300 tỷ là thầy xin Giáo hội cho hoàn tục!"
+        },
         prob: 0.8
       },
       { content: { text: "Tầm này rồi, liêm sỉ gì nữa!" }, prob: 0.2 }
+    ]
+  },
+  {
+    input_contains: ["cầu", "ước", "muốn", "mong", "xin"],
+    responses: [
+      {
+        content: {
+          text: "Con cầu cái đấy thì Khá Bảnh cũng khá đuối nữa là thầy"
+        },
+        prob: 0.25
+      },
+      {
+        content: {
+          text: "Con cầu cái gì dễ dễ như hoà bình thế giới được không"
+        },
+        prob: 0.25
+      },
+      {
+        content: { text: "Có thờ có thiêng, có tiền mới nhanh con ạ" },
+        prob: 0.25
+      },
+      { content: { text: "Cầu gì cầu nhảm vậy con?" }, prob: 0.25 }
     ]
   }
 ];
 
 /*If cannot find appropriate responses, defer to random responses*/
 const stock = [
-  { content: initialResponse, prob: 0.8 },
+  { content: initialResponse, prob: 0.4 },
   {
     content: {
       text: "Chả hiểu con nói gì. Thôi thứ nhất là tu tại gia, con về đi"
     },
-    prob: 0.2
+    prob: 0.3
+  },
+  {
+    content: {
+      text: "Đi thẳng vào vấn đề nào con, nhà bao việc!"
+    },
+    prob: 0.3
   }
 ];
 
